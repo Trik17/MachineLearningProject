f57250b840054e753292a892691359b1e04ae3c6
Trik17 <andreatricarico17@gmail.com>
2018-11-29 00:00:27 -0600

    import data & first preprocessing

--- Util.py
+++ Util.py
@@ -4,42 +4,23 @@ import pandas as pd
 # numerical computation
 import numpy as np
 
-import re
+class Dataset:
 
-# visualization library
-import seaborn as sns
-sns.set(style="white", color_codes=True)
-sns.set_context(rc={"font.family":'sans',"font.size":24,"axes.titlesize":24,"axes.labelsize":24})
+    #static pseidoPrivate:
+    #__prova = 5
+    #static public:
+    #prova = 5
+
+    def __init__(self):
+        # Reading the data
+        self.dataset = pd.read_csv('responses.csv')
+        self.X = self.dataset.drop(columns=['Empathy'])
+        self.Y = self.dataset['Empathy']
 
 
-# import matplotlib and allow it to plot inline
-import matplotlib.pyplot as plt
-#%matplotlib inline
 
-#for k-Means
-from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
 
-from scipy.spatial.distance import cdist, pdist
-# seaborn can generate several warnings, we ignore them
-import warnings
-warnings.filterwarnings("ignore")
-
-from bokeh.layouts import gridplot
-from bokeh.plotting import figure
-
-from bokeh.io import output_notebook, show
-from bokeh.models import ColumnDataSource
-output_notebook()
-
-from datetime import datetime, timedelta, date
-from scipy.stats import skew
-from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, ElasticNet, Lasso, LassoCV
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
-from sklearn.datasets import load_boston
-
-import statsmodels.api as sm
+#with this it is possible to run the code only if this class is runned
+#(otherwise it is runned everytime that the class is imported)
+#if __name__ == "__main__":
 
